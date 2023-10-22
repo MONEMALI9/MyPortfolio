6482ec48a6efb044c6dfd500c2ebb4a590106568
MONEMALI9 <monem921999@gmail.com>
2023-10-22 15:55:19 +0300

    .

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 
 function openLinks() {
-    // Open the GitHub link in a new tab
-    window.open("https://github.com/MONEMALI9/Examination_System", "_blank");
-    
-    // Open the PDF link in a new tab
-    window.open("./ABDELMONEM ALI .pdf", "_blank");
+    // Prompt the user for a password
+    var userPassword = prompt("Please enter the password:");
+
+    // Check if the password is correct
+    if (userPassword === "monem") {
+        // If the password is correct, open the GitHub link in a new tab
+        window.open("https://github.com/MONEMALI9/Examination_System", "_blank");
+
+        // Open the PDF link in a new tab
+        window.open("./ABDELMONEM ALI .pdf", "_blank");
+    } else {
+        alert("Incorrect password. Access denied.");
+    }
 }
