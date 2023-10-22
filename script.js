c9613de18230ee25d617cfcd7bda2d51a949cf73
MONEMALI9 <monem921999@gmail.com>
2023-10-22 16:04:28 +0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-function openLinks() {
+function powerbi() {
     // Prompt the user for a password
     var userPassword = prompt("Please enter the password:");
 
@@ -14,3 +14,46 @@ function openLinks() {
         alert("Incorrect password. Access denied.");
     }
 }
+
+function aws() {
+    // Prompt the user for a password
+    var userPassword = prompt("Please enter the password:");
+
+    // Check if the password is correct
+    if (userPassword === "monem") {
+        // If the password is correct, open the GitHub link in a new tab
+        window.open("https://github.com/MONEMALI9/AwsCodeBuildRole", "_blank");
+
+        // Open the PDF link in a new tab
+        window.open("./education/AWS ResStart.pdf", "_blank");
+    } else {
+        alert("Incorrect password. Access denied.");
+    }
+}
+
+function bachelorDegree() {
+    // Prompt the user for a password
+    var userPassword = prompt("Please enter the password:");
+
+    // Check if the password is correct
+    if (userPassword === "monem") {
+        // If the password is correct, open the GitHub link in a new tab
+        window.open("https://github.com/MONEMALI9/AwsCodeBuildRole", "_blank");
+
+        // Open the PDF link in a new tab
+        window.open("./education/شهاده مؤقته.pdf", "_blank");
+
+        // Open the PDF link in a new tab
+        window.open("./education/ترجمة الشهادة المؤقته.pdf ", "_blank");
+
+        // Open the PDF link in a new tab
+        window.open("./ABDELMONEM ALI .pdf", "_blank");
+
+        // Open the PDF link in a new tab
+        window.open("./ABDELMONEM ALI .pdf", "_blank");
+    } else {
+        alert("Incorrect password. Access denied.");
+    }
+}
+
+
