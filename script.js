b1ffec58c2551f58ebbd4951d17da9b933df99bb
MONEMALI9 <monem921999@gmail.com>
2023-10-22 23:19:43 +0300

    .

--- script.js
+++ script.js
@@ -49,3 +49,37 @@ function bachelorDegree() {
         alert("Incorrect password. Access denied.");
     }
 }
+// Function to open links related to a bachelor's degree
+function fwd() {
+    // Prompt the user for a password
+    var userPassword = prompt("Please enter the password:");
+
+    // Check if the password is correct
+    if (userPassword === "monem") {
+        // If the password is correct, open various links in new tabs
+        window.open("./scholarship/challenger", "_blank");
+        window.open("./scholarship/challenger", "_blank");
+        window.open("./scholarship/challenger", "_blank");
+        window.open("./scholarship/challenger", "_blank");       
+    } else {
+        alert("Incorrect password. Access denied.");
+    }
+}
+
+// Function to open links related to a bachelor's degree
+function fwd() {
+    // Prompt the user for a password
+    var userPassword = prompt("Please enter the password:");
+
+    // Check if the password is correct
+    if (userPassword === "monem") {
+        // If the password is correct, open various links in new tabs
+        window.open("https://github.com/MONEMALI9/AwsCodeBuildRole", "_blank");
+        window.open("./education/شهاده مؤقته.pdf", "_blank");
+        window.open("./education/ترجمة الشهادة المؤقته.pdf", "_blank");
+        window.open("./ABDELMONEM ALI .pdf", "_blank");
+        window.open("./ABDELMONEM ALI .pdf", "_blank");
+    } else {
+        alert("Incorrect password. Access denied.");
+    }
+}
