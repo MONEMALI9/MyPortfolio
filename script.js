53ce9d871acf9e412eaa420d98327e791a34d238
MONEMALI9 <monem921999@gmail.com>
2023-10-22 16:08:10 +0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// Function to open Power BI links
 function powerbi() {
     // Prompt the user for a password
     var userPassword = prompt("Please enter the password:");
@@ -15,6 +15,7 @@ function powerbi() {
     }
 }
 
+// Function to open AWS links
 function aws() {
     // Prompt the user for a password
     var userPassword = prompt("Please enter the password:");
@@ -31,29 +32,20 @@ function aws() {
     }
 }
 
+// Function to open links related to a bachelor's degree
 function bachelorDegree() {
     // Prompt the user for a password
     var userPassword = prompt("Please enter the password:");
 
     // Check if the password is correct
     if (userPassword === "monem") {
-        // If the password is correct, open the GitHub link in a new tab
+        // If the password is correct, open various links in new tabs
         window.open("https://github.com/MONEMALI9/AwsCodeBuildRole", "_blank");
-
-        // Open the PDF link in a new tab
         window.open("./education/شهاده مؤقته.pdf", "_blank");
-
-        // Open the PDF link in a new tab
-        window.open("./education/ترجمة الشهادة المؤقته.pdf ", "_blank");
-
-        // Open the PDF link in a new tab
+        window.open("./education/ترجمة الشهادة المؤقته.pdf", "_blank");
         window.open("./ABDELMONEM ALI .pdf", "_blank");
-
-        // Open the PDF link in a new tab
         window.open("./ABDELMONEM ALI .pdf", "_blank");
     } else {
         alert("Incorrect password. Access denied.");
     }
 }
-
-
